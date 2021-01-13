56c9b23a91070365520cefac25c13ef65465e3c0
MURA199918 <muralisaranr18@gmail.com>
2021-01-13 13:18:03 +0530

    [Murali]Refactored to retrieve data from JSON Server

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/homePage.html",
     add_address_book_form_page: "../pages/AddressBookForm.html",
     server_url: "http://localhost:3000/AddressBookDB"
